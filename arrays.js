8d90c1b69a9cada763a0a374e0eee0a507698bc4
MARCO RIZZUTO <mprizzuto88@gmail.com>
2019-10-09 16:01:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -51,6 +51,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 function removeElementFromEndOfArray(array){
   //return array.slice(array.length - 1);
    // return array.slice(array.length - 1);
-return array.slice(array.length);
-
+   
 }
